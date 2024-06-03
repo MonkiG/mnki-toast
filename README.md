e04723b597baaf3fb76e0177955426e365be2d06
MonkiG <raan.heam@gmail.com>
2024-06-03 11:46:53 -0600

    add: update README and test toasts in main.js

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Standalone built in web components toast component
 
 ## Todo
 
-- [🕥] handle the multiple constructors and connecteds either Constructor, create element, inner element
 - [x] Handle multiple toasts expand them
 - [ ] Handle multiple toasts stack them
-- [ ] Add multiple styles of toasts (success, warn, error, default)
 - [ ] Add multiple types of toasts (default, action, etc...)
+- [ ] Handle responsive
 
-## Idea
-Probably it is a better idea add the toast to a container to handle in a better way the reposition of the toasts,
-just appended it to the container and remove it from the container when the time expires but I have to refactor all the code
+Declarative:
+- [x] insertAdjacent
+- [x] append
+- [ ] innerHtml
--- web/main.js
+++ web/main.js
@@ -1,12 +1,41 @@
 import toast from '@monkig/mnki-toast'
-const colors = ['success', 'error', 'warn']
+const colors = ['success', 'error', 'warn', 'info']
 
 for (let i = 0; i <= colors.length; i++) {
   toast('Single toast title', { color: colors[i] })
+  toast('Single toast title', { color: colors[i], icon: false })
   toast('Single toast title', {
     color: colors[i],
     position: 'bot-left',
     description: 'Non labore irure excepteur laborum veniam est culpa laboris in sint voluptate et aute.Tempor dolor voluptate ipsum aute labore.'
-
   })
 }
+
+document.body.insertAdjacentHTML('afterbegin', `
+<mnki-toast
+  data-color="error"
+  data-text="Declarative insert toast"
+  data-position="top-left"
+  data-time="2000"
+
+>
+</mnki-toast>`)
+
+const myToast = document.createElement('mnki-toast')
+myToast.setAttribute('data-text', 'Imperative toast')
+myToast.setAttribute('data-position', 'top-left')
+myToast.setAttribute('data-color', 'warn')
+myToast.setAttribute('data-icon', 'false')
+
+document.body.append(myToast)
+
+// I must handle this case
+// document.body.innerHTML = `
+//  <mnki-toast
+//   data-color="success"
+//   data-text="Declarative inner toast"
+//   data-position="top-left"
+//   data-time="2000"
+//  >
+//  </mnki-toast>
+// `
